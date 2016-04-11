5504a7b3f71f714b3553665fe305d1fd4f9205ab
aliafshar250 <mehdizare800@mailfa.com>
2016-04-11 15:03:21 +0430

    Create lock_audio.lua

---
+++ plugins/lock_audio.lua
@@ -0,0 +1,17 @@
+
+local function run(msg)
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)]['settings']['lock_audo'] == 'yes' then
+--    if msg.media then 
+  --  if not msg.media.captin == "sticker.webp"  then
+    if not is_momod(msg) then
+        delete_msg(msg.id, ok_cb, true)
+        return 
+    end
+  end
+end
+
+return {patterns = {
+    '%[(audio)%]',
+    '%[(document)%]',
+    }, run = run}
