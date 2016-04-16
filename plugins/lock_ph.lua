c4028e5196128e73b4cd7fb9fceeb1719a878e4b
aliafshar250 <mehdizare800@mailfa.com>
2016-04-16 21:12:24 +0430

    Update lock_ph.lua

--- plugins/lock_ph.lua
+++ plugins/lock_ph.lua
@@ -1,14 +1,30 @@
-local function run(msg)
-    local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)]['settings']['lock_ph'] == 'yes' then
-    if not is_momod(msg) then
-        delete_msg(msg.id, ok_cb, true)
-        return 
-    end
-  end
-end
 
-return {patterns = {
-    '%[(document)%]',
-    '%[(photo)%]',
-    }, run = run}
+
+
+local function run(msg)
+    
+    local data = load_data(_config.moderation.data)
+    
+     if data[tostring(msg.to.id)]['settings']['lock_ph'] == 'yes' then
+      
+    
+if not is_momod(msg) then
+    
+    
+chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msgads = 'ForbiddenAdText'
+   local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
+	
+      end
+   end
+end
+    
+return {
+patterns = {
+"%[(photo)%]",
+"%[(document)%]",
+"%[(unsupported)%]",
+},
+ run = run
+}
