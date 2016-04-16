21b2f283b94b239da2f7260507baacd850b7ecc4
aliafshar250 <mehdizare800@mailfa.com>
2016-04-16 21:11:32 +0430

    Update lock_audio.lua

--- plugins/lock_audio.lua
+++ plugins/lock_audio.lua
@@ -1,17 +1,31 @@
 
-local function run(msg)
-    local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)]['settings']['lock_audio'] == 'yes' then
---    if msg.media then 
-  --  if not msg.media.captin == "sticker.webp"  then
-    if not is_momod(msg) then
-        delete_msg(msg.id, ok_cb, true)
-        return 
-    end
-  end
-end
 
-return {patterns = {
-    '%[(audio)%]',
-    '%[(document)%]',
-    }, run = run}
+
+local function run(msg)
+    
+    local data = load_data(_config.moderation.data)
+    
+     if data[tostring(msg.to.id)]['settings']['lock_audio'] == 'yes' then
+      
+    
+if not is_momod(msg) then
+    
+    
+chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msgads = 'ForbiddenAdText'
+   local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
+	
+      end
+   end
+end
+    
+return {
+patterns = {
+
+"%[(document)%]",
+"%[(audio)%]",
+"%[(unsupported)%]",
+},
+ run = run
+}
