eed0b80f1854b2a8a8fb5e1841acc0ceee30896d
aliafshar250 <mehdizare800@mailfa.com>
2016-04-17 01:34:38 +0430

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,14 +245,28 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {145173448,137791771,},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
-https://github.com/BH-YAGHI/NOD32-BOT.git
-
-channel : @Nod32team
-sodu : @behrooZyaghi
+MEGA KING
+—---------------------------------
+A NEW BOT FOR MANAGE YOUR SUPERGROUPS.
+—---------------------------------
+@TEAM_KING_BOT
+—---------------------------------
+@mohammad20162015
+—---------------------------------
+@mehdisudo 
+—---------------------------------
+TNX FOR @SORBLACK AND @alireza_PT
+—---------------------------------
+CREATE TEAM BY MOHAMMAD
+—---------------------------------
+Bot version : 1.1 
+----------------------------------
+To shop groups be admins Recourse
+the
 ]],
     help_text_realm = [[
 Realm Commands:
