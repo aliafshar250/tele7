9122ff83250edf56ef8d2c2b915645aa8bd14be3
aliafshar250 <mehdizare800@mailfa.com>
2016-04-17 01:51:40 +0430

    Update filterworld.lua

--- plugins/filterworld.lua
+++ plugins/filterworld.lua
@@ -1,144 +1,143 @@
 local function save_filter(msg, name, value)
-  local hash = nil
-  if msg.to.type == 'chat' then
-    hash = 'chat:'..msg.to.id..':filters'
-  end
-  if msg.to.type == 'user' then
-    return 'فقط در گروه ممکن است'
-  end
-  if hash then
-    redis:hset(hash, name, value)
-    return "انجام شد"
-  end
+	local hash = nil
+	if msg.to.type == 'chat' then
+		hash = 'chat:'..msg.to.id..':filters'
+	end
+	if msg.to.type == 'user' then
+		return nil
+	end
+	if hash then
+		redis:hset(hash, name, value)
+	return "Word blocked on "  ..string.gsub(msg.to.print_name, "_", " ")
+	end
 end
 
 local function get_filter_hash(msg)
-  if msg.to.type == 'chat' then
-    return 'chat:'..msg.to.id..':filters'
-  end
+	if msg.to.type == 'chat' then
+		return 'chat:'..msg.to.id..':filters'
+	end
 end 
 
 local function list_filter(msg)
-  if msg.to.type == 'user' then
-    return 'فقط در گروه'
-  end
-  local hash = get_filter_hash(msg)
-  if hash then
-    local names = redis:hkeys(hash)
-    local text = 'لیست کلمات فیلتر شده:\n______________________________\n'
-    for i=1, #names do
-      text = text..'> '..names[i]..'\n'
-    end
-    return text
-  end
+	if msg.to.type == 'user' then
+		return nil
+	end
+	local hash = get_filter_hash(msg)
+	if hash then
+		local names = redis:hkeys(hash)
+		local text = "Filterlist for "  ..string.gsub(msg.to.print_name, "_", " ").." \n===========================\n"
+		for i=1, #names do
+			text = text..'> '..names[i]..'\n'
+		end
+		return text
+	end
 end
 
 local function get_filter(msg, var_name)
-  local hash = get_filter_hash(msg)
-  if hash then
-    local value = redis:hget(hash, var_name)
-    if value == 'msg' then
-      return 'کلمه ی کاربردی شما ممنوع است، در صورت تکرار با شما برخورد خواهد شد'
-    elseif value == 'kick' then
-      send_large_msg('chat#id'..msg.to.id, "به دلیل عدم رعایت قوانین گفتاری از ادامه ی گفتوگو محروم میشوید")
-      chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    end
-  end
+	local hash = get_filter_hash(msg)
+	if hash then
+		local value = redis:hget(hash, var_name)
+		if value == 'msg' then
+			return 'This word is block in this chat.'
+		elseif value == 'kick' then
+			send_large_msg('chat#id'..msg.to.id, "you useged block word.bye bye")
+			chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+		end
+	end
 end
 
 local function get_filter_act(msg, var_name)
-  local hash = get_filter_hash(msg)
-  if hash then
-    local value = redis:hget(hash, var_name)
-    if value == 'msg' then
-      return 'اخطار و تذکر به این کلمه'
-    elseif value == 'kick' then
-      return 'این کلمه ممنوع است و حذف خواهید شد'
-    elseif value == 'none' then
-      return 'این کلمه از فیلتر خارج شده است'
-    end
-  end
+	local hash = get_filter_hash(msg)
+	if hash then
+		local value = redis:hget(hash, var_name)
+		if value == 'msg' then
+			return 'danger!'
+		elseif value == 'kick' then
+			return 'kick!'
+		elseif value == 'none' then
+			return 'not block this word!'
+		end
+	end
 end
 
 local function run(msg, matches)
-  local data = load_data(_config.moderation.data)
-  if matches[1] == "ilterlist" then
-    return list_filter(msg)
-  elseif matches[1] == "ilter" and matches[2] == ">" then
-    if data[tostring(msg.to.id)] then
-      local settings = data[tostring(msg.to.id)]['settings']
-      if not is_momod(msg) then
-        return "َشما دسترسی ندارید"
-      else
-        local value = 'msg'
-        local name = string.sub(matches[3]:lower(), 1, 1000)
-        local text = save_filter(msg, name, value)
-        return text
-      end
-    end
-  elseif matches[1] == "ilter" and matches[2] == "+" then
-    if data[tostring(msg.to.id)] then
-      local settings = data[tostring(msg.to.id)]['settings']
-      if not is_momod(msg) then
-        return "َشما دسترسی ندارید"
-      else
-        local value = 'kick'
-        local name = string.sub(matches[3]:lower(), 1, 1000)
-        local text = save_filter(msg, name, value)
-        return text
-      end
-    end
-  elseif matches[1] == "ilter" and matches[2] == "-" then
-    if data[tostring(msg.to.id)] then
-      local settings = data[tostring(msg.to.id)]['settings']
-      if not is_momod(msg) then
-        return "َشما دسترسی ندارید"
-      else
-        local value = 'none'
-        local name = string.sub(matches[3]:lower(), 1, 1000)
-        local text = save_filter(msg, name, value)
-        return text
-      end
-    end
-  elseif matches[1] == "ilter" and matches[2] == "?" then
-    return get_filter_act(msg, matches[3]:lower())
-  else
-    if is_sudo(msg) then
-      return
-    elseif is_admin(msg) then
-      return
-    elseif is_momod(msg) then
-      return
-    elseif tonumber(msg.from.id) == tonumber(our_id) then
-      return
-    else
-      return get_filter(msg, msg.text:lower())
-    end
-  end
+	local data = load_data(_config.moderation.data)
+	if matches[1] == "ilterlist" then
+		return list_filter(msg)
+	elseif matches[1] == "ilter" and matches[2] == ">" then
+		if data[tostring(msg.to.id)] then
+			local settings = data[tostring(msg.to.id)]['settings']
+			if is_momod(msg) then
+				return "Only moderators can do it!"
+			else
+				local value = 'msg'
+				local name = string.sub(matches[3]:lower(), 1, 1000)
+				local text = save_filter(msg, name, value)
+				return text
+			end
+		end
+	elseif matches[1] == "ilter" and matches[2] == "+" then
+		if data[tostring(msg.to.id)] then
+			local settings = data[tostring(msg.to.id)]['settings']
+			if not is_momod(msg) then
+				return "Only moderators can do it!"
+			else
+				local value = 'kick'
+				local name = string.sub(matches[3]:lower(), 1, 1000)
+				local text = save_filter(msg, name, value)
+				return text
+			end
+		end
+	elseif matches[1] == "ilter" and matches[2] == "-" then
+		if data[tostring(msg.to.id)] then
+			local settings = data[tostring(msg.to.id)]['settings']
+			if not is_momod(msg) then
+				return "Only moderators can do it!"
+			else
+				local value = 'none'
+				local name = string.sub(matches[3]:lower(), 1, 1000)
+				local text = save_filter(msg, name, value)
+				return text
+			end
+		end
+	elseif matches[1] == "ilter" and matches[2] == "?" then
+		return get_filter_act(msg, matches[3]:lower())
+	else
+		if is_sudo(msg) then
+			return
+		elseif is_admin(msg) then
+			return
+		elseif is_momod(msg) then
+			return
+		elseif tonumber(msg.from.id) == tonumber(our_id) then
+			return
+		else
+			return get_filter(msg, msg.text:lower())
+		end
+	end
 end
 
 return {
-  description = "Set and Get Variables", 
-  usage = {
-  user = {
-    "filter ? (word) : مشاهده عکس العمل",
-    "filterlist : لیست فیلتر شده ها",
-  },
-  moderator = {
-    "filter > (word) : اخطار کردن لغت",
-    "filter + (word) : ممنوع کردن لغت",
-    "filter - (word) : حذف از فیلتر",
-  },
-  },
-  patterns = {
-    "^[Ff](ilter) (.+) (.*)$",
-    "^[Ff](ilterlist)$",
-    "(.*)",
-  },
-  run = run
+	description = "Set and Get Variables", 
+	usagehtm = '<tr><td align="center">filter > </td><td align="right"></td></tr>'
+	..'<tr><td align="center">filter + </td><td align="right"></td></tr>'
+	..'<tr><td align="center">filter - </td><td align="right">'
+	..'<tr><td align="center">filter ? </td><td align="right"></td></tr>',
+	usage = {
+	user = {
+		"filter ? (word) : ",
+		"filterlist : ",
+	},
+	moderator = {
+		"filter > (word) : ",
+		"filter + (word) : ",
+		"filter - (word) : ",
+	},
+	},
+	patterns = {
+		"^[Ff](ilter) (.+) (.*)$",
+		"^[Ff](ilterlist)$",
+		"(.*)",
+	},
+	run = run
 }
-
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
